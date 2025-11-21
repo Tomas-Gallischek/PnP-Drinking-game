8439d87ecf5098a86f536dc460cb01d10b7f6919
Tomas Gallischek <t.gall@seznam.cz>
2025-11-21 08:37:10 +0100

    pití

--- game/models.py
+++ game/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 
 class player(models.Model):
-    name = models.CharField(max_length=100)
-    lvl = models.IntegerField(default=0)
-    xp = models.IntegerField(default=0)
+    name = models.CharField(max_length=100, blank=True)
+    lvl = models.IntegerField(default=0, blank=True)
+    xp = models.IntegerField(default=0, blank=True)
+    panak = models.IntegerField(default=0, blank=True)
+    maly_kelimek = models.IntegerField(default=0, blank=True)
+    velky_kelimek = models.IntegerField(default=0, blank=True)
     
 
     def __str__(self):
--- game/views.py
+++ game/views.py
@@ -1,18 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect # Přidat 'redirect'
 from .models import player
 
 
-def drink(request):
-    player_id = request.GET.get('player_id')
-    one_player = player.objects.get(id=player_id)   
+def drink(request, player_id):
+    one_player = player.objects.get(id=player_id)
 
-    return render(request, 'game/player_info.html', {
-        'one_player': one_player})
+    if request.method == 'POST':
+        drink_type = request.POST.get('drink_type')
+        print(drink_type)
+        if drink_type == 'panak':
+            one_player.xp += 30
+            one_player.panak += 1
+        elif drink_type == 'maly_kelimek':
+            one_player.xp += 10
+            one_player.maly_kelimek += 1
+        elif drink_type == 'velky_kelimek':
+            one_player.xp += 20
+            one_player.velky_kelimek += 1
+        one_player.save()
 
+        return redirect('player_info', player_id=one_player.id)
+    
 
-
-
-
+    return redirect('player_info', player_id=one_player.id)
 
 
 def player_info(request, player_id):
