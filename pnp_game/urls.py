21364580c9eb6c635d171701800cccd9bb096439
Tomas Gallischek <t.gall@seznam.cz>
2025-12-04 14:05:41 +0100

    zkouška pozadi css

--- pnp_game/urls.py
+++ pnp_game/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from . import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -9,3 +11,6 @@ urlpatterns = [
     path('fightapp/', include('fightapp.urls')),
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
