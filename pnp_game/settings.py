b464e48c1a1d76f31b76a6c098a78887253dc3a8
Tomas Gallischek <t.gall@seznam.cz>
2025-12-27 06:14:05 +0100

    oprava static

--- pnp_game/settings.py
+++ pnp_game/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -121,7 +122,7 @@ STATICFILES_DIRS = [
 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
